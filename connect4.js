feb1dc8403b82029e9a8f98aae863a064fb4896d
dharvSpring <116044877+dharvSpring@users.noreply.github.com>
2022-11-08 18:22:14 -0600

    functionally complete

--- connect4.js
+++ connect4.js
@@ -8,7 +8,7 @@
 const WIDTH = 7;
 const HEIGHT = 6;
 
-const currPlayer = 1; // active player: 1 or 2
+let currPlayer = 1; // active player: 1 or 2
 const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
@@ -16,15 +16,24 @@ const board = []; // array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  // set "board" to empty HEIGHT x WIDTH matrix array
+  for (let y = 0; y < HEIGHT; y++) {
+    board[y] = new Array(WIDTH);
+    // fill the row because every doesn't run on empty spaces
+    for (let x = 0; x < WIDTH; x++) {
+      board[y][x] = 0;
+    }
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  // get "htmlBoard" variable from the item in HTML w/ID of "board"
+  const htmlBoard = document.querySelector('#board');
 
-  // TODO: add comment for this code
+  // creates the top row and adds event listener
+  // for the players to choose a column on their turn
   const top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
@@ -36,7 +45,8 @@ function makeHtmlBoard() {
   }
   htmlBoard.append(top);
 
-  // TODO: add comment for this code
+  // creates the html tr and td for the board and
+  // sets the id to match the coordinates of the square
   for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
     for (var x = 0; x < WIDTH; x++) {
@@ -51,20 +61,33 @@ function makeHtmlBoard() {
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  // due to building, bottom index is HEIGHT
+  for (let y = HEIGHT; y--; y > -1) {
+    if (board[y][x] === 0) {
+      return y;
+    }
+  }
+  return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
+  // make a div and insert into correct table cell
+  // querySelector doesn't like numbers at the start of ids
+  const playTd = document.getElementById(`${y}-${x}`);
+
+  const pieceDiv = document.createElement('div');
+  pieceDiv.classList.add(`p${currPlayer}`);
+  pieceDiv.classList.add('piece');
+
+  playTd.append(pieceDiv);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+  alert(msg); // TODO do better
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -80,7 +103,7 @@ function handleClick(evt) {
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
+  board[y][x] = currPlayer;
   placeInTable(y, x);
 
   // check for win
@@ -89,10 +112,16 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
+  if (checkForDraw()) {
+    return endGame("It's a draw!");
+  }
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  currPlayer = (currPlayer === 1) ? 2 : 1;
+}
+
+function checkForDraw() {
+  return board.every((row) => (row.every((cell) => (cell !== 0))));
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -113,8 +142,9 @@ function checkForWin() {
     );
   }
 
-  // TODO: read and understand this code. Add comments to help you.
-
+  // iterates over each cell and checks for a win
+  // checks horizontal, vertical, and both diagonal
+  // only checks 'forward' because previous iterations have already checked back
   for (let y = 0; y < HEIGHT; y++) {
     for (let x = 0; x < WIDTH; x++) {
       const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
